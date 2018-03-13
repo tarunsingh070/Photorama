7864bd0b13d6426aa04564428b307fd3446f34ca
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2018-03-12 21:33:13 -0400

    Completed The final version of the Photorama app.

---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Tarun Singh on 2018-03-12.
+//  Copyright © 2018 Tarun Singh. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+}
